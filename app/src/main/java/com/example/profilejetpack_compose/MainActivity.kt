c41a250ce6b00bccc0b8806c0881a8e0db7641ae
YadavYashvant <yashvantyadav855@gmail.com>
2023-07-16 15:03:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/profilejetpack_compose/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.profilejetpack_compose
+
+import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.profilejetpack_compose.ui.theme.ProfileJetpackComposeTheme
+
+
+val fontfamily = R.font.delagothicone_regular
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    ProfileScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileScreen() {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(10.dp)) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            
+            Text(text = "Cancel", modifier = Modifier
+                .clickable { }
+                .padding(10.dp))
+            Text(text = "Save", modifier = Modifier
+                .clickable { }
+                .padding(10.dp))
+            
+        }
+
+        ProfileImage()
+        
+    }
+}
+
+@Composable
+fun ProfileImage() {
+    val imageUri = rememberSaveable{ mutableStateOf("") }
+    val painter = rememberImagePainter(
+        if(imageUri.value.isEmpty())
+            R.drawable.person_pfp
+        else
+            imageUri.value
+    )
+    
+    Column(modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth(),
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        Card(shape = CircleShape,
+            modifier = Modifier
+                .padding(15.dp)
+                .size(150.dp)
+            ) {
+            Image(painter = painter, contentDescription = null, modifier = Modifier
+                .wrapContentSize()
+                .clickable { },
+                contentScale = ContentScale.Crop
+                )
+        }
+        Text(text = "Change Profile Picture", modifier = Modifier.padding(10.dp))
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProfileJetpackComposeTheme {
+        ProfileScreen()
+    }
+}
