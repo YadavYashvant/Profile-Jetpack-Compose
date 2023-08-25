7d1c73d209398a1e64909272148f4b441dc7fce6
YadavYashvant <yashvantyadav855@gmail.com>
2023-08-25 07:57:14 +0530

    added a loader animation

--- app/src/main/java/com/example/profilejetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/profilejetpack_compose/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.profilejetpack_compose
 
 import android.content.Intent
+import android.content.Loader
+import android.graphics.drawable.GradientDrawable
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -34,20 +36,41 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+
 import com.example.profilejetpack_compose.ui.theme.ProfileJetpackComposeTheme
 
 
@@ -62,6 +85,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     ProfileScreen()
+                    Loaderbouncy()
                 }
             }
         }
@@ -72,43 +96,21 @@ class MainActivity : ComponentActivity() {
 fun ProfileScreen() {
     Column(modifier = Modifier
         .verticalScroll(rememberScrollState())
-        .padding(8.dp)
+        .padding(10.dp)
         ) {
         Row(modifier = Modifier
             .fillMaxWidth()
             .padding(6.dp)
-            .height(40.dp),
+            .height(60.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-
-
-            /*
-            Text(text = "Cancel", modifier = Modifier
-                .clickable { }
-                .padding(20.dp)
-                .background(color = Color.Black)
-            ,color = Color.White
-                , fontWeight = FontWeight.Bold
-            )
-            */
-
-
-            ElevatedButton(onClick = { /*TODO*/ }, modifier = Modifier
+            ElevatedButton(onClick = { /*TODO*/
+                                     
+                                     }, modifier = Modifier
                 .fillMaxHeight()
             ) {
                 Text(text = "Cancel")
             }
-
-
-            /*
-            Text(text = "Save", modifier = Modifier
-                .clickable { }
-                .padding(20.dp)
-                .background(color = Color.Magenta)
-                ,color = Color.White
-                , fontWeight = FontWeight.Bold)
-             */
-
 
             OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier
                 .fillMaxHeight()
@@ -163,6 +165,65 @@ fun ProfileImage() {
         Text(text = "Profile Picture", modifier = Modifier.padding(10.dp),fontFamily = fontfamily1, fontWeight = FontWeight.Medium)
     }
 }
+
+@Composable
+fun Loaderbouncy() {
+    val yOffset = remember{
+        Animatable(0f)
+    }
+     val scale = remember{
+         Animatable(1f)
+     }
+
+    LaunchedEffect("Loader") {
+        delay(400)
+        yOffset.animateTo(.5f,bouncyAnimationSpecific)
+        scale.animateTo(3f,bouncyAnimationSpecific)
+
+        delay(500)
+        scale.animateTo(10f,bouncyAnimationSpecific)
+        delay(500)
+        scale.animateTo(50f,bouncyAnimationSpecific)
+    }
+    val size =  remember{
+        mutableStateOf(IntSize.Zero)
+    }
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .onSizeChanged {
+                size.value = it
+            }
+    ) {
+        GradientCircle(
+            Modifier
+                .align(Alignment.TopCenter)
+                .size(26.dp)
+                .graphicsLayer {
+                    scaleX = scale.value
+                    scaleY = scale.value
+                    translationY = yOffset.value * size.value.height
+                }
+        )
+    }
+}
+
+@Composable
+fun GradientCircle(modifier: Modifier = Modifier) {
+    val brush = remember{
+        Brush.verticalGradient(listOf(Color(0xFFF56E34), Color(0xFF234EDA)))
+    }
+
+    Canvas(modifier = modifier) {
+        drawCircle(brush = brush)
+    }
+}
+
+val bouncyAnimationSpecific: SpringSpec<Float> = spring(
+    dampingRatio = Spring.DampingRatioMediumBouncy,
+    stiffness = Spring.StiffnessVeryLow
+)
 
 
 @Preview(showBackground = true)
